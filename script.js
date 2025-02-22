1b753c88f16c6a899468bbd59cd2dffebca2fd10
Kevin McCarthy <ave_astro@Kevins-MacBook-Air.local>
2025-02-22 16:24:53 +0900

    Committed

--- script.js
+++ script.js
@@ -1,6 +1,6 @@
 window.addEventListener('scroll', function() {
     let scrollTop = window.pageYOffset || document.documentElement.scrollTop;
-    let speed = 0.2; // Adjust scrolling speed
+    let speed = 0.5; // Adjust scrolling speed
     let asterism = document.querySelector('.asterism');
 
     // Calculate the asterism's vertical position with parallax effect
