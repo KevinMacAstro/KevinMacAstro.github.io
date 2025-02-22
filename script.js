fee3f8ceb05cacf6cab186703af2c17667576368
Kevin McCarthy <ave_astro@Kevins-MacBook-Air.local>
2025-02-22 16:16:04 +0900

    Committed

--- script.js
+++ script.js
@@ -1,14 +1,9 @@
 window.addEventListener('scroll', function() {
     let scrollTop = window.pageYOffset || document.documentElement.scrollTop;
-    let speed = 0.3; // Adjust scrolling speed
+    let speed = 0.2; // Adjust scrolling speed
     let asterism = document.querySelector('.asterism');
 
     // Calculate the asterism's vertical position with parallax effect
-    let yPosition = -1*scrollTop * speed;
-    asterism.style.top = `${yPosition}px`;
-
-    //Ensure that it doesn't go off the top of the screen.
-    if (yPosition < 0) {
-        asterism.style.top = '0px';
-    }
+    let yPosition = -scrollTop * speed; // Negative for upward movement
+    asterism.style.transform = `translateY(${yPosition}px)`;
 });
