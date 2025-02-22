3ed3eae2648dc9bf427c40e5824ad53d9284a932
Kevin McCarthy <ave_astro@Kevins-MacBook-Air.local>
2025-02-22 16:47:10 +0900

    Committed

--- script.js
+++ script.js
@@ -1,10 +1,13 @@
 window.addEventListener('scroll', function() {
     let scrollTop = window.pageYOffset || document.documentElement.scrollTop;
+    let speed = 0.2; // Adjust scrolling speed
     let asterism = document.querySelector('.asterism');
-    let maxBrightness = 2.5; // Maximum brightness level
+    let maxBrightness = 2.0; // Maximum brightness level
     let minBrightness = 0.5;   // Minimum brightness level
-    let scrollHeight = document.body.scrollHeight - window.innerHeight; //added to scale the brightness
+    let scrollHeight = document.body.scrollHeight - window.innerHeight;
     let brightness = minBrightness + (maxBrightness - minBrightness) * (scrollTop / scrollHeight);
+    let yPosition = -scrollTop * speed;
 
+    asterism.style.backgroundPositionY = `${yPosition}px`;
     asterism.style.filter = `brightness(${brightness})`;
 });
