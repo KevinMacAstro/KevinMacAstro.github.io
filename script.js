77de504c4760c0a3261e876b23f857c3b5dc7453
KSM <kevin.mccarthy@ipmu.jp>
2025-08-01 16:20:51 +0900

    Committed.

--- script.js
+++ script.js
@@ -1,17 +1,25 @@
-window.addEventListener('scroll', function() {
+document.addEventListener("DOMContentLoaded", function () {
+  let asterism = document.querySelector('.asterism');
+  if (!asterism) return;
+
+  window.addEventListener('scroll', function () {
     let scrollTop = window.scrollY || document.documentElement.scrollTop;
-    let speed = 0.4; // Adjust scrolling speed - Increased for better visual effect
-    let asterism = document.querySelector('.asterism');
-    let maxBrightness = 1.2; // Maximum brightness level
-    let minBrightness = 0.5; // Minimum brightness level
+    let speed = 0.4;
+    let maxBrightness = 1.2;
+    let minBrightness = 0.5;
     let scrollHeight = document.body.scrollHeight - window.innerHeight;
-    
-    // Calculate the brightness using a sine wave function
+
     let brightness = minBrightness + (maxBrightness - minBrightness) * (0.5 * (1 + Math.sin((scrollTop / scrollHeight) * 2 * Math.PI)));
-    let yPosition = -scrollTop * speed; // Control background position with scroll
+    let yPosition = -scrollTop * speed;
 
     asterism.style.backgroundPositionY = `${yPosition}px`;
     asterism.style.filter = `brightness(${brightness})`;
+  });
+
+  const contentContainer = document.querySelector('.content-container');
+  contentContainer.style.display = 'flex';
+  contentContainer.style.justifyContent = 'center';
+  contentContainer.style.alignItems = 'center';
 });
 
 
